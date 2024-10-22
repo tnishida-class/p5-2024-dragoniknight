6379be3d5506156121607643ed5d2744540502f5
dragoniknight <dragon.city.8002@gmail.com>
2024-10-22 12:11:41 +0900

    change section3-3

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,12 +1,14 @@
-let x, y, vy;
+let x, y, vy,a,b;
 const g = 1;
 const vyMax = 20;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
+  x = windowWidth / 2;
   y = windowHeight-125;
   vy = 2;
+  a = windowWidth;
+  b = windowHeight;
 }
 
 function draw(){
@@ -16,17 +18,23 @@ function draw(){
   rect(0,windowHeight-100,windowWidth,100)
   fill(50)
   ellipse(x, y, 50);
+  fill(100)
+  rect(a,b-300,50)
+　fill(255)
+　textSize(30)
+  text("？",a+10,b-265)
   y+= vy;
   vy+= g*1/100;
   vy = constrain(vy, vyMax, -vyMax);
   y = constrain(y, 0, height-125);
   let i=0
-  if(keyIsDown("A".charCodeAt(0))){ i = 1; }
-  if(keyIsDown(LEFT_ARROW)){ x -= (i+1)*10; }
-  if(keyIsDown(RIGHT_ARROW)){ x += (i+1)*10; }
+  if(keyIsDown("A".charCodeAt(0))){ i = 1.5; }
+  if(keyIsDown("Z".charCodeAt(0))){ i = -0.5; }
+  if(keyIsDown(LEFT_ARROW)){ a += (i+1)*10; }
+  if(keyIsDown(RIGHT_ARROW)){ a -= (i+1)*10; }
 }
 function keyPressed(){
-  if(y>=height-125){
+  if(y==height-125){
    if(keyIsDown(" ".charCodeAt(0))){ y-= vy*10; vy-= -g*1/100}
   }
 }
