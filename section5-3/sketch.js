41037b51a635fba1918ebc961d7edfd8879b5787
dragoniknight <dragon.city.8002@gmail.com>
2024-11-06 12:14:33 +0900

    change section5-3

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,10 +1,10 @@
-// テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
-  calendar(2019, 10);
+  createCanvas(2000, 2000);
+  daysInYear(2003);
+  dayOfWeek(2024, 11, 5)
+  calendar(2024,11)
 
-  // isLeapYear の動作確認のため console に出力しています
-  for(let i = 2000; i <= 2100; i++){
+  for(let i = 2003; i <= 2024; i++){
     if(isLeapYear(i)){
       console.log(i + "年はうるう年です");
     }
@@ -15,9 +15,38 @@ function setup(){
 }
 
 function calendar(y, m){
-  let dow = dayOfWeek(y, m, 1);
+  rect(50,0,350,50)
+  text(m+"月",210,30)
+  for(let b = 0; b < 7; b++){
+   for(let c = 0; c < 6; c++){
+    if(1<b<5){
+     fill(255)
+     rect(50+b*50,50,50,25)
+     rect(50+b*50, 75+c*50, 50)
+    }
+    if(b == 0){
+     fill(255,100,100)
+     rect(50,50,50,25)
+     fill(255,200,200)
+     rect(50+b*50, 75+c*50, 50)
+    }
+    if(b == 6){
+     fill(100,100,255)
+     rect(350,50,50,25)
+     fill(200,200,255)
+     rect(50+b*50, 75+c*50, 50)
+    }
+   }
+  }
+  let p = 0
   for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+   let dow = dayOfWeek(y, m, d);
+   if(dow == 0){
+     p = p+1
+   }
+   fill(0)
+   text(d,55+dow*50,90+p*50)
+   text(dayOfWeekAsString(dow),70+dow*50,67)
   }
 }
 
@@ -26,7 +55,14 @@ function isLeapYear(y){
 }
 
 function daysInYear(y){
-  // BLANK[1]
+  for(let y = 2003; y <= 2024; y++){
+  　if(isLeapYear(y)){
+    　return 366 
+  　}
+  　else{
+    　return 365
+  　}
+  }
 }
 
 function daysInMonth(y, m){
@@ -50,10 +86,13 @@ function dayOfYear(y, m, d){
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+  let x = 1
+  x += dayOfYear(y, m, d)
+  x -= dayOfYear(1970,1,1)
+  return x % 7
 }
 
 function dayOfWeekAsString(dow){
-  const a = ["日", "月", "火", "水", "木", "金", "土", "日"];
+  const a = ["日", "月", "火", "水", "木", "金", "土"];
   return a[dow];
 }
